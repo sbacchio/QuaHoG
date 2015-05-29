638f08938c4f43d8829cbf6a2ed506be614ea9e8
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2015-05-29 18:11:58 +0200

    Fixed calculation of local volume in three-point hdf5 writing function

--- lib/qhg_write_nn_thrp.c
+++ lib/qhg_write_nn_thrp.c
@@ -68,7 +68,7 @@ qhg_write_nn_thrp(char fname[], qhg_thrp_correlator corr_thrp)
       ldims[0] = lt-t0%lt;
     
     if(pc[0] == t_proc_ts)
-      ldims[0] = ldims[0] - (lt-ts%lt)%lt + 1;
+      ldims[0] = ldims[0] - lt-ts%lt + 1;
 
     if((pc[0] == t_proc_ts) && (pc[0] == t_proc_t0))
       ldims[0] = dt+1;
@@ -86,7 +86,6 @@ qhg_write_nn_thrp(char fname[], qhg_thrp_correlator corr_thrp)
       starts[0] = 0;
     }
   }
-
   
   hid_t fapl_id = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD, MPI_INFO_NULL);
