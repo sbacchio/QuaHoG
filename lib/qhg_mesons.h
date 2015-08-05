df4d34d3be8d8f68b69ecbc92e02e85415c53cb8
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2015-08-05 15:03:16 +0300

    Cleaned up mesons header file

--- lib/qhg_mesons.h
+++ lib/qhg_mesons.h
@@ -4,6 +4,5 @@
 #include <qhg_types.h>
 
 qhg_correlator qhg_mesons(qhg_spinor_field [], qhg_spinor_field [], int []);
-void qhg_write_mesons(char [], qhg_correlator);
   
 #endif /* _QHG_MESONS_H */
