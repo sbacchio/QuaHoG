76348f556f96f8d5f74348434410b39fdb79f28f
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2015-11-24 14:58:59 +0200

    Added additional functions on propagators, including transposition of only color indices, color traces, etc. used in baryon contractions.

--- lib/qhg_prop_mul.h
+++ lib/qhg_prop_mul.h
@@ -1,6 +1,7 @@
 #ifndef _QHG_PROP_MUL_H
 #define _QHG_PROP_MUL_H 1
 #include <complex.h>
+#include <qhg_idx.h>
 
 static void
 prop_mul_gg(_Complex double Z[NS*NC][NS*NC], _Complex double X[NS*NC][NS*NC], _Complex double Y[NS*NC][NS*NC])
@@ -46,6 +47,26 @@ prop_mul_dd(_Complex double Z[NS*NC][NS*NC], _Complex double X[NS*NC][NS*NC], _C
       Z[cs0][cs1] = 0;
       for(int cs2=0; cs2<NC*NS; cs2++)
 	Z[cs0][cs1] += conj(X[cs2][cs0])*conj(Y[cs1][cs2]);
+    }
+  return;
+}
+
+/*
+ * This is not your standard matrix-matrix multiplication. Returns:
+ * Z^{a b}_{mu nu} = sum_{k, c} X^{ac}_{mu nu} Y^{cb}_{k k}
+ */
+static void
+prop_mul_gtr(_Complex double Z[NS*NC][NS*NC], _Complex double X[NS*NC][NS*NC], _Complex double Y[NS*NC][NS*NC])
+{
+  for(int sp0=0; sp0<NS; sp0++)
+    for(int sp1=0; sp1<NS; sp1++)
+      for(int col0=0; col0<NC; col0++)
+	for(int col1=0; col1<NC; col1++) {
+	  Z[CS(sp0, col0)][CS(sp1, col1)] = 0.0;
+	  for(int sp2=0; sp2<NS; sp2++)
+	    for(int col2=0; col2<NC; col2++)
+	      Z[CS(sp0, col0)][CS(sp1, col1)] +=
+		X[CS(sp0, col0)][CS(sp1, col2)]*Y[CS(sp2, col2)][CS(sp2, col1)];
     }
   return;
 }
--- lib/qhg_prop_ops.h
+++ lib/qhg_prop_ops.h
@@ -2,7 +2,7 @@
 #define _QHG_PROP_OPS_H 1
 
 #define VSC(v, sc) (sc + NC*NS*v)
-
+#include <qhg_idx.h>
 #include <qhg_prop_mul.h>
 #include <qhg_prop_mul_su3.h>
 #include <qhg_prop_contract.h>
@@ -85,6 +85,66 @@ prop_zero(_Complex double A[NC*NS][NC*NS])
     for(int cs1=0; cs1<NS*NC; cs1++) {
       A[cs0][cs1] = 0.0;
     }
+  return;
+}
+
+static void
+prop_transpose(_Complex double A[NC*NS][NC*NS])
+{
+  for(int csp0=0; csp0<NC*NS-1; csp0++)
+    for(int csp1=csp0+1; csp1<NC*NS; csp1++) {
+      _Complex double swap = A[csp0][csp1];
+      A[csp0][csp1] = A[csp1][csp0];
+      A[csp1][csp0] = swap;
+    }
+
+  return;
+}
+
+static void
+prop_color_transpose(_Complex double A[NC*NS][NC*NS])
+{
+  for(int sp0=0; sp0<NS; sp0++)
+    for(int sp1=0; sp1<NS; sp1++)    
+      for(int col0=0; col0<NC-1; col0++)
+	for(int col1=col0+1; col1<NC; col1++) {
+	  _Complex double swap = A[CS(sp0, col0)][CS(sp1, col1)];
+	  A[CS(sp0, col0)][CS(sp1, col1)] = A[CS(sp0, col1)][CS(sp1, col0)];
+	  A[CS(sp0, col1)][CS(sp1, col0)] = swap;
+	}
+  return;
+}
+
+static void
+prop_color_trace(_Complex double T[NS][NS], _Complex double A[NC*NS][NC*NS])
+{
+  for(int sp0=0; sp0<NS; sp0++)
+    for(int sp1=0; sp1<NS; sp1++) {
+      T[sp0][sp1] = 0.0;
+      for(int col0=0; col0<NC; col0++) {
+	int cs0 = col0 + sp0*NC;
+	int cs1 = col0 + sp1*NC;
+	T[sp0][sp1] += A[cs0][cs1];
+      }
+    }
+  return;
+}
+
+static void
+prop_spinor_transpose(_Complex double A[NC*NS][NC*NS])
+{
+  for(int sp0=0; sp0<NS-1; sp0++)
+    for(int sp1=sp0+1; sp1<NS; sp1++)    
+      for(int col0=0; col0<NC; col0++)
+	for(int col1=0; col1<NC; col1++) {
+	  int cs0a = col0 + sp0*NC;
+	  int cs1a = col1 + sp1*NC;
+	  int cs0b = col0 + sp1*NC;
+	  int cs1b = col1 + sp0*NC;
+	  _Complex double swap = A[cs0a][cs1a];
+	  A[cs0a][cs1a] =  A[cs0b][cs1b];
+	  A[cs1b][cs1b] = swap;
+	}
   return;
 }
 
