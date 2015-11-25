c7e4c94f3a51b1dd1d71e59d3005b8ab1c4a6f8f
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2015-11-25 11:18:52 +0200

    Checked out qhg_comms.c from master

--- lib/qhg_comms.c
+++ lib/qhg_comms.c
@@ -16,36 +16,47 @@ qhg_comms_init(qhg_lattice *lat)
   int verbosity = 3;
   tmLQCD_invert_init(0, NULL, verbosity);
 
-  tmLQCD_lat_params lp;
-  tmLQCD_mpi_params mp;   
-  tmLQCD_get_lat_params(&lp);
-  tmLQCD_get_mpi_params(&mp);  
-  
-  int ldims[ND] = {lp.T, lp.LX, lp.LY, lp.LZ};  
-  int procs[ND] = {mp.nproc_t, mp.nproc_x, mp.nproc_y, mp.nproc_z};
+  tmLQCD_lat_params *lp = qhg_alloc(sizeof(tmLQCD_lat_params));
+  tmLQCD_mpi_params *mp = qhg_alloc(sizeof(tmLQCD_mpi_params));
+  int ret = 0;
+  ret = tmLQCD_get_lat_params(lp);
+  ret = tmLQCD_get_mpi_params(mp);
+
+  int ldims[ND] = {lp->T, lp->LX, lp->LY, lp->LZ};
+  int procs[ND] = {mp->nproc_t, mp->nproc_x, mp->nproc_y, mp->nproc_z};
   int gdims[ND], lvol=1, gvol=1;
   for(int i=0; i<ND; i++) {
     gdims[i] = ldims[i]*procs[i];
     lvol *= ldims[i];
     gvol *= gdims[i];
   }
-  
+
   for(int dir=0; dir<ND; dir++)
     if(gdims[dir] != lat->dims[dir]) {
       fprintf(stderr, " Dimension missmatch with tmLQCD\n");
+      fprintf(stderr, " tmLQCD	: %d*%d %d*%d %d*%d %d*%d\n",
+	      lp->T,procs[0],
+	      lp->LX,procs[1],
+	      lp->LY,procs[2],
+	      lp->LZ,procs[3]);
+      fprintf(stderr, " QHG	: %d %d %d %d\n",
+	      lat->dims[0],
+	      lat->dims[1],
+	      lat->dims[2],
+	      lat->dims[3]);
       exit(1);
     }
-  qhg_comms *comms = qhg_alloc(sizeof(qhg_comms));  
+  qhg_comms *comms = qhg_alloc(sizeof(qhg_comms));
   for(int dir=0; dir<ND; dir++) {
     comms->proc_dims[dir] = procs[dir];
-    comms->proc_coords[dir] = mp.proc_coords[dir];
+    comms->proc_coords[dir] = mp->proc_coords[dir];
   }
-  comms->proc_id = mp.cart_id;
-  comms->nprocs = mp.nproc;
+  comms->proc_id = mp->cart_id;
+  comms->nprocs = mp->nproc;
 
 #ifdef QHG_OMP
-  comms->nthreads = mp.omp_num_threads;
-  omp_set_num_threads(comms->nthreads);  
+  comms->nthreads = mp->omp_num_threads;
+  omp_set_num_threads(comms->nthreads);
 #pragma omp parallel
   {
     int ithr = omp_get_thread_num();
@@ -54,8 +65,8 @@ qhg_comms_init(qhg_lattice *lat)
   }
 #else
   comms->nthreads = 1;
-#endif   
-  
+#endif
+
   for(int dir=0; dir<ND; dir++) {
     int c[ND];
 
@@ -87,4 +98,3 @@ qhg_comms_finalize(qhg_comms *comms)
   MPI_Finalize();
   return;
 }
-
