0edd268100c8aa6fa58ba35fef6f2bdf5a59e8f5
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2016-02-12 16:50:22 +0100

    tmLQCD structs are now allocated rather than statically declared

--- lib/qhg_comms.c
+++ lib/qhg_comms.c
@@ -16,13 +16,14 @@ qhg_comms_init(qhg_lattice *lat)
   int verbosity = 3;
   tmLQCD_invert_init(0, NULL, verbosity);
 
-  tmLQCD_lat_params lp;
-  tmLQCD_mpi_params mp;   
-  tmLQCD_get_lat_params(&lp);
-  tmLQCD_get_mpi_params(&mp);  
+  tmLQCD_lat_params *lp = qhg_alloc(sizeof(tmLQCD_lat_params));
+  tmLQCD_mpi_params *mp = qhg_alloc(sizeof(tmLQCD_mpi_params));
+  int ret = 0;
+  ret = tmLQCD_get_lat_params(lp);
+  ret = tmLQCD_get_mpi_params(mp);  
   
-  int ldims[ND] = {lp.T, lp.LX, lp.LY, lp.LZ};  
-  int procs[ND] = {mp.nproc_t, mp.nproc_x, mp.nproc_y, mp.nproc_z};
+  int ldims[ND] = {lp->T, lp->LX, lp->LY, lp->LZ};  
+  int procs[ND] = {mp->nproc_t, mp->nproc_x, mp->nproc_y, mp->nproc_z};
   int gdims[ND], lvol=1, gvol=1;
   for(int i=0; i<ND; i++) {
     gdims[i] = ldims[i]*procs[i];
@@ -33,18 +34,28 @@ qhg_comms_init(qhg_lattice *lat)
   for(int dir=0; dir<ND; dir++)
     if(gdims[dir] != lat->dims[dir]) {
       fprintf(stderr, " Dimension missmatch with tmLQCD\n");
+      fprintf(stderr, " tmLQCD	: %d*%d %d*%d %d*%d %d*%d\n",
+	      lp->T,procs[0],
+	      lp->LX,procs[1],
+	      lp->LY,procs[2],
+	      lp->LZ,procs[3]);
+      fprintf(stderr, " QHG	: %d %d %d %d\n",
+	      lat->dims[0],
+	      lat->dims[1],
+	      lat->dims[2],
+	      lat->dims[3]);
       exit(1);
     }
   qhg_comms *comms = qhg_alloc(sizeof(qhg_comms));  
   for(int dir=0; dir<ND; dir++) {
     comms->proc_dims[dir] = procs[dir];
-    comms->proc_coords[dir] = mp.proc_coords[dir];
+    comms->proc_coords[dir] = mp->proc_coords[dir];
   }
-  comms->proc_id = mp.cart_id;
-  comms->nprocs = mp.nproc;
+  comms->proc_id = mp->cart_id;
+  comms->nprocs = mp->nproc;
 
 #ifdef QHG_OMP
-  comms->nthreads = mp.omp_num_threads;
+  comms->nthreads = mp->omp_num_threads;
   omp_set_num_threads(comms->nthreads);  
 #pragma omp parallel
   {
