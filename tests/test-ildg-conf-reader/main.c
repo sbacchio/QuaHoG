9208b48fe0335ea4a5258baadfab13ad76e678ef
Giannis Koutsou <g.koutsou@cyi.ac.cy>
2016-04-01 16:10:59 +0300

    Modified ILDG conf. reader test program for compatibility with new communication initialisation

--- tests/test-ildg-conf-reader/main.c
+++ tests/test-ildg-conf-reader/main.c
@@ -1,4 +1,4 @@
-#include <mpi.h>
+tests/test-ildg-conf-reader/invert.input#include <mpi.h>
 #include <qhg.h>
 
 void
@@ -20,9 +20,18 @@ main(int argc, char *argv[])
   asprintf(&fname, "%s", argv[1]);
   
   int dims[ND] = {12, 6, 8, 10}; // t,x,y,z
-  qhg_lattice *lat = qhg_lattice_init(dims);
+  qhg_comms *comms = qhg_comms_init(NULL);
+  qhg_lattice *lat = qhg_lattice_init(dims, comms);
   int am_io_proc = lat->comms->proc_id == 0 ? 1 : 0;
   qhg_gauge_field gf = qhg_gauge_field_init(lat);  
+
+  if(am_io_proc)
+    printf(" nproc = %d :: (%d,%d,%d,%d)\n",
+	   comms->nprocs,
+	   comms->proc_dims[0],
+	   comms->proc_dims[1],
+	   comms->proc_dims[2],
+	   comms->proc_dims[3]);
   
   /*
     Read config assuming ildg
@@ -34,10 +43,11 @@ main(int argc, char *argv[])
   */
   double p = qhg_plaquette(gf);
   if(am_io_proc)
-    printf("Plaquette = %10.8f\n", p);
+    printf(" Plaquette = %10.8f\n", p);
 
   qhg_gauge_field_finalize(gf);  
   qhg_lattice_finalize(lat);
+  qhg_comms_finalize(comms);
   return 0;
 }
   
